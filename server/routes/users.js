f705cc30b592a52cf406851ebd8f67fa04642eea
saurabhgdeshmukh <saurabhgdeshmukh@gmail.com>
2023-10-06 23:14:23 +0530

    User Rutes setup

---
+++ server/routes/users.js
@@ -0,0 +1,16 @@
+import { Express } from "express";
+import{
+    getUser,
+    getUserFriends,
+    addRemoveFriend,
+} from "../controllers/users.js"
+import { verifyToken } from "../middleware/auth.js";
+
+const router=express.Router();
+
+
+router.get("/:id",verifyToken,getUser);
+router.get("/:id",verifyToken,getUserFriends);
+
+router.patch("/:id",verifyToken,addRemoveFriend);
+export default router;
