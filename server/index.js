e0887d53033e97238c1d76a2be888bdb7921fb66
saurabhgdeshmukh <saurabhgdeshmukh@gmail.com>
2023-10-19 22:58:41 +0530

    Adding back end data

--- server/index.js
+++ server/index.js
@@ -62,8 +62,8 @@ mongoose
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
-    /* ADD DATA ONE TIME */
-    // User.insertMany(users);
-    // Post.insertMany(posts);
+    
+     User.insertMany(users);
+     Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
